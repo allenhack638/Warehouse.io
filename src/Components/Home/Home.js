5a68e4d85473cec9d07f2d642744a260425258ed
Allen Benny <allenbenny038@gmail.com>
2023-09-30 13:29:23 +0530

    added navbar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import GoogleSheetTable from "../GoogleSheetTable/GoogleSheetTable";
+import "./Home.css";
 
 export const Home = () => {
   return (
     <div>
+      <nav className="navbar">
+        <h1 className="nav-heading">Sheet to Table</h1>
+      </nav>
       <GoogleSheetTable />
     </div>
   );
